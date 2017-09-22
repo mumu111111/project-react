c6f514a830e7a8d982e6f2c44bb892b5e5c3e62d
mumu111111 <2646709035@qq.com>
2017-09-22 12:29:45 +0800

    '组件'

--- src/index.js
+++ src/index.js
@@ -4,17 +4,33 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import Welcome from './Welcome'
 
-function tick(){
-    const element=(
-        <div>
-          <h1>Hello world</h1>
-          <h2>It is {new Date().toLocalTimerString()}.</h2>
-        </div>  
-    );
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
-    );
-}
-setInterval(tick, 1000);
+
+
+
+
+ReactDOM.render(
+    <Welcome/>,
+    document.getElementById('root2')
+)
+
+// function tick(){
+//     const element=(
+//         <div>
+//           <h1>Hello world</h1>
+//           <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//         </div>  
+//     );
+//     ReactDOM.render(
+//         element,
+//         document.getElementById('root2')
+//     );
+// }
+//  setInterval(tick, 1000);
+// //tick()
+
+
+
+
+
