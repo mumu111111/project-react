5f8f4d7d5fc2518bdef4a26efc1e12d47ca954a4
mumu111111 <2646709035@qq.com>
2017-09-22 11:15:04 +0800

    'index.js'

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-    <h1>Hir, world!</h1>,
-    document.getElementById('root2')
-);
+
+function tick(){
+    const element=(
+        <div>
+          <h1>Hello world</h1>
+          <h2>It is {new Date().toLocalTimerString()}.</h2>
+        </div>  
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    );
+}
+setInterval(tick, 1000);
