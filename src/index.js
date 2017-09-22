6fc01557ce808dd71dc4c318da05b38f52b4f21f
娟 <2646709035@qq.com>
2017-09-22 11:20:50 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-function tick(){
-    const element=(
-        <div>
-          <h1>Hello world</h1>
-          <h2>It is {new Date().toLocalTimerString()}.</h2>
-        </div>  
-    );
+
     ReactDOM.render(
-        element,
+       <h1>hi  world</h1>,
         document.getElementById('root2')
     );
-}
-setInterval(tick, 1000);
+
