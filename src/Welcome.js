b5b0684d73933114a0f0063cb0db6678e5f19ad6
mumu111111 <2646709035@qq.com>
2017-09-23 17:28:12 +0800

    state

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,9 +1,28 @@
 import  React from 'react';
 
 class Welcome extends React.Component{
+   
+    constructor(props){
+        super(props)
+
+        this.state={
+            date: new Date()
+        }
+    }
+   
+   
+   
+   
     render(){
         
-        return <h1>{this.props.name}}</h1>;
+        
+        return (
+
+            <div>
+                <h1>Hello, {this.props.name}</h1>
+                <h2>{this.state.date.toString()}</h2>
+            </div>    
+        )
     }
 }
 
