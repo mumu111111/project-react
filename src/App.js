c0d1a1668fcd3ad9d09c4ea0221e2166ff762fa7
mumu111111 <2646709035@qq.com>
2017-09-24 15:32:58 +0800

    将输入框编程TodoInput 组件

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import TodoInput from './TodoInput'
 
 class App extends Component {
 
@@ -25,7 +26,7 @@ let todos = this.state.todoList.map((item, index)=>{
     <div className='App'>
       <h1>我的待办</h1>
       <div className='inputWrapper'>
-        <input type='text' value={this.state.newTodo}/>
+        <TodoInput content={this.state.newTodo} />
       </div>
       <ol>
         {todos}
