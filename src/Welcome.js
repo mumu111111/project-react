4d2a75c7e5e26dd973f17ec95de05b700a000da8
mumu111111 <2646709035@qq.com>
2017-09-24 14:04:48 +0800

    确定哪些地方可以setState

--- src/Welcome.js
+++ src/Welcome.js
@@ -6,37 +6,69 @@ class Welcome extends React.Component{
         super(props)
 
         this.state={
-            date: new Date()
+            date: new Date(),
+            test: '1'
         }
         setInterval(()=>{
             this.setState({
-                date: new Date()
+                date: new Date(),  //更新date
+                test: 'setInterval'
             })
-        })
+        },5000)
+
         console.log('我已经在constuctor 里将 props 和 state 被初始化好了')
     }
    
    
    componentWillMount(){
+       
        console.log('运行到这里的话， 说明马上就要运行render了')
    }
    
-    render(){
-        
-
-        console.log('这里是render， 将返回的值 插入到 页面')
-        return (
-
-            <div>
-                <h1>Hello, {this.props.name}</h1>
-                <h2>{this.state.date.toString()}</h2>
-            </div>    
-        )
-    }
+   render() {
+    
+    console.log('嗯，这里是 render')
+    return (
+      <div>
+        <h1>Hello, {this.props.name}</h1>
+        <h2>{this.state.date.toString()}</h2>
+        <p>{this.state.test}</p>
+      </div>
+    )
+  }
 
 componentDidMount(){
+   
 
     console.log('已经挂载在页面里了')
+}
+
+componentWillReceiveProps(){// 更新 date
+
+    this.setStste({
+        date: new Date(),
+        test: 'componentWillReceiveProps'
+    })
+}
+
+shouldComponentUpdate(){
+    this.setState({
+        date: new Date(), //更新date
+        test: 'shouldComponentUpdate'
+    })
+    return true
+}
+
+compoentWillUpdate(){
+
+}
+
+componentDidUpdate(){
+    
+}
+
+componentWillUnmount(){
+    console.log('要死了')
 }
 
 }
