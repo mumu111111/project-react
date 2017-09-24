94b87e982123a647b6aeb70be779db893de53399
mumu111111 <2646709035@qq.com>
2017-09-24 15:12:57 +0800

    基本骨架完成

--- src/index.js
+++ src/index.js
@@ -9,29 +9,12 @@ ReactDOM.render(
     document.getElementById('root2')
 )
 
-// function tick(){
-//     const element=(
-//         <div>
-//           <h1>Hello world</h1>
-//           <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//         </div>  
-//     );
-//     ReactDOM.render(
-//         element,
-//         document.getElementById('root2')
-//     );
-// }
-//  setInterval(tick, 1000);
-// //tick()
 
 
 
 
 
 
-    // ReactDOM.render(
-    //    <h1>hi  world</h1>,
-    //     document.getElementById('root2')
-    // );
 
+  
 
