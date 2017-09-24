1928614834e9c0a1b74b064965d468545a83717e
mumu111111 <2646709035@qq.com>
2017-09-24 09:08:18 +0800

    mount

--- src/Welcome.js
+++ src/Welcome.js
@@ -8,14 +8,23 @@ class Welcome extends React.Component{
         this.state={
             date: new Date()
         }
+        setInterval(()=>{
+            this.setState({
+                date: new Date()
+            })
+        })
+        console.log('我已经在constuctor 里将 props 和 state 被初始化好了')
     }
    
    
-   
+   componentWillMount(){
+       console.log('运行到这里的话， 说明马上就要运行render了')
+   }
    
     render(){
         
-        
+
+        console.log('这里是render， 将返回的值 插入到 页面')
         return (
 
             <div>
@@ -24,6 +33,12 @@ class Welcome extends React.Component{
             </div>    
         )
     }
+
+componentDidMount(){
+
+    console.log('已经挂载在页面里了')
+}
+
 }
 
 export default Welcome
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
 
 import Welcome from './Welcome'
-
-
-
-
 
 ReactDOM.render(
     <Welcome name="frank"/>,
