29667d0e2e6a5046fa0aecbbf9f1e07bf71b4e27
mumu111111 <2646709035@qq.com>
2017-09-28 16:14:29 +0800

    测试leancloud 功能

--- src/App.js
+++ src/App.js
@@ -16,6 +16,13 @@ AV.init({
 });
 
 
+var TestObject = AV.Object.extend('TestObject');
+var testObject = new TestObject();
+testObject.save({
+  words: 'Hello World!'
+}).then(function(object) {
+  alert('LeanCloud Rocks!');
+})
 
 
 class App extends Component {
