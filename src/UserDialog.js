d7981bfde84cdbb2517be3085dccebb7bea1b1d2
mumu111111 <2646709035@qq.com>
2017-09-30 12:30:58 +0800

    signInOrSignUp抽离成一个组件

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -4,6 +4,7 @@ import {signUp, signIn, sendPasswordResetEmail} from './leanCloud'
 import SignUpForm from './SignUpForm'
 import SignInForm from './SignInForm'
 import  ForgotPasswordForm from './ForgotPasswordForm'
+import SignInOrSignUp from './SignInOrSignUp'
 
 export default class UserDialog extends Component{
 
@@ -85,40 +86,7 @@ export default class UserDialog extends Component{
        
 
 
-        let signInOrSignUp= (
-            <div className="signInOrSignUp">
-            
-                <nav >
-                    <label><input type="radio" value="signUp"
-                    checked={this.state.selected==='signUp'}
-                    onChange={this.switch.bind(this)}
-                    />注册
-                    </label>
-                    <label><input type="radio" value="signIn"
-                    checked={this.state.selected==="signIn"}
-                    onChange={this.switch.bind(this)}
-                    />登录
-                    </label>
-                </nav>
-                <div className="panes">
-                    {this.state.selected === 'signUp' ?
-                    
-                     <SignUpForm formData ={this.state.formData}
-                       onSubmit={this.signUp.bind(this)}
-                       onChange={this.changFormData.bind(this)}/>
-                     : null}
-
-                     
-                    {this.state.selected === 'signIn' ? 
-                       <SignInForm formData={this.state.formData}
-                         onSubmit={this.signIn.bind(this)}
-                         onChange={this.changFormData.bind(this)}
-                         onForgotPassword={this.showForgotPassword.bind(this)}/>
-                         
-                     : null}
-                </div>
-        </div>
-        )
+        
 
 
         
@@ -127,7 +95,15 @@ export default class UserDialog extends Component{
             <div className="UserDialog-Wrapper">
                 <div className="UserDialog">
                     {this.state.selectedTab ==='signInOrSignUp' ?
-                              signInOrSignUp : 
+                    
+             
+                            <SignInOrSignUp
+                                formData={this.state.formData}
+                                onSignIn={this.signIn.bind(this)}
+                                onSignUp={this.signUp.bind(this)}
+                                onChange={this.changFormData.bind(this)}
+                                onForgotPassword={this.showForgotPassword.bind(this)}
+                            /> :
                               <ForgotPasswordForm 
                                 formData={this.state.formData}
                                 onSubmit={this.resetPassword.bind(this)}
