d7e558fef82304da1b828cdca75ced5f844f0069
mumu111111 <2646709035@qq.com>
2017-10-01 07:39:00 +0800

    如果用户在输入框里什么都不写就敲回车， 那么就拒绝用户

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -7,10 +7,11 @@ import './TodoInput.css'
 
 function submit(props, e){
     
-     
             if(e.key=== 'Enter'){
-    
-               props.onSubmit(e)
+                if(e.target.value.trim()  !== ''){
+
+                    props.onSubmit(e)
+                }
             }
         }
     
